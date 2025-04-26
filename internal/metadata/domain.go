17d169257c65dbabd57ae0c7da60390527938f2d
Lean998 <leandroaguero83@gmail.com>
2025-04-26 20:24:20 -0300

    Update domain.go
    Correcciones para el campo approved

--- internal/metadata/domain.go
+++ internal/metadata/domain.go
@@ -3,7 +3,7 @@ package metadata
 // Metadata represents a system sale with metadata for auditing and versioning.
 type Metadata struct {
 	Quantity     int     `json:"quantity"`
-	Approved     int     `json:"approve"`
+	Approved     int     `json:"approved"`
 	Pending      int     `json:"pending"`
 	Rejected     int     `json:"rejected"`
 	Total_amount float32 `json:"total_amount"`
